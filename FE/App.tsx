f3e7e81c9b4c4802efbdbbdd80d262e427b9c0b8
rhtjddls123 <jjjk0605@naver.com>
2024-04-16 04:41:44 +0900

    Feat: drawer navigation구현

--- FE/App.tsx
+++ FE/App.tsx
@@ -1,5 +1,66 @@
 import LoginScreen from "./screens/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./screens/HomeScreen";
+import { Image, Text, View, StyleSheet } from "react-native";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <LoginScreen />;
+  const login = { login: true };
+  return (
+    <>
+      <NavigationContainer>
+        {login.login ? (
+          <Drawer.Navigator
+            screenOptions={{
+              headerTitleAlign: "center",
+              drawerItemStyle: { backgroundColor: "white" },
+              drawerActiveTintColor: "#27C077",
+              headerTintColor: "black",
+            }}
+          >
+            <Drawer.Screen
+              name="NAITE"
+              component={HomeScreen}
+              options={{
+                headerTitle: () => (
+                  <View style={styles.title}>
+                    <Text style={styles.titleText}>NAITE</Text>
+                    <Image
+                      style={styles.logo}
+                      source={require("./assets/images/logo_no_title.png")}
+                    />
+                  </View>
+                ),
+                title: "진단하기",
+              }}
+            />
+            {/* <Drawer.Screen name="Login" component={LoginScreen} /> */}
+          </Drawer.Navigator>
+        ) : (
+          <LoginScreen />
+        )}
+      </NavigationContainer>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  logo: {
+    width: 44,
+    height: 44,
+  },
+  header: {
+    margin: 0,
+  },
+});

06280565fa7103127486e3a9288ee053e29cf9a1
unknown <jjjk0605@naver.com>
2024-05-21 18:03:41 +0900

    Feat: 사진 여러장 선택가능하도록 변경

--- FE/components/CameraButton.tsx
+++ FE/components/CameraButton.tsx
@@ -1,107 +1,143 @@
-import { Alert, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 import {
-  PermissionStatus,
-  launchCameraAsync,
-  launchImageLibraryAsync,
-  useCameraPermissions,
-} from "expo-image-picker";
+  Alert,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Modal,
+  Dimensions,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
 import axios, { AxiosResponse } from "axios";
 import { URI } from "@env";
 import { useNavigation } from "@react-navigation/native";
 import { result } from "../types/type";
 
-type Props = {
+interface Props {
   label: string;
   loadingHandler: (bool: boolean) => void;
   email: string;
-};
+}
 
-const CameraButton = ({ label, email, loadingHandler }: Props) => {
-  console.log(URI);
-  const [cameraPermissionInformation, requestPermission] =
-    useCameraPermissions();
+interface SelectedImage {
+  uri: string;
+  base64: string;
+}
+
+const CameraButton: React.FC<Props> = ({ label, email, loadingHandler }) => {
   const navigation = useNavigation();
+  const [selectedImages, setSelectedImages] = useState<SelectedImage[]>([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const screenHeight = Dimensions.get("window").height;
 
-  async function verifyPermissions() {
-    if (cameraPermissionInformation?.status === PermissionStatus.UNDETERMINED) {
-      const permissionResponse = await requestPermission();
-      return permissionResponse.granted;
-    }
-
-    if (cameraPermissionInformation?.status === PermissionStatus.DENIED) {
+  async function verifyPermissions(): Promise<boolean> {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status !== "granted") {
       Alert.alert(
         "Insufficient Permissions!",
         "You need to grant camera permissions to use this app"
       );
       return false;
     }
-
     return true;
   }
 
-  async function takeImageHandler() {
+  async function takeImagesWithCamera() {
     const hasPermission = await verifyPermissions();
-
     if (!hasPermission) {
       return;
     }
 
     loadingHandler(true);
 
-    const image = await launchCameraAsync({
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-      base64: true,
-    });
-
-    if (!image.canceled && image.assets[0].base64) {
-      await uploadImage(image.assets[0].base64);
-    } else {
-      loadingHandler(false);
-    }
-  }
-
-  async function pickImageHandler() {
-    const hasPermission = await verifyPermissions();
-
-    if (!hasPermission) {
-      return;
-    }
-
-    loadingHandler(true);
-
-    const image = await launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-      base64: true,
-    });
-
-    if (!image.canceled && image.assets[0].base64) {
-      await uploadImage(image.assets[0].base64);
-    } else {
-      loadingHandler(false);
-    }
-  }
-
-  async function uploadImage(base64: string) {
     try {
-      const result = await axios
-        .post(`${URI}/image`, {
-          file: base64,
-          label: label,
-          email: email,
-        })
-        .then((res: AxiosResponse<result>) => res.data);
-      console.log(result);
+      const imageResult = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+        base64: true,
+      });
+
+      if (!imageResult.canceled && imageResult.assets) {
+        setSelectedImages((prevImages) => [
+          ...prevImages,
+          ...imageResult.assets.map((asset) => ({
+            uri: asset.uri,
+            base64: asset.base64 || "",
+          })),
+        ]);
+      }
+      setModalVisible(true);
+    } catch (error) {
+      console.log("Error taking images: ", error);
+    } finally {
+      loadingHandler(false);
+    }
+  }
+
+  async function selectImagesFromGallery() {
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant media library permissions to use this app"
+      );
+      return;
+    }
+
+    loadingHandler(true);
+
+    try {
+      const imageResult = await ImagePicker.launchImageLibraryAsync({
+        allowsMultipleSelection: true,
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        base64: true,
+      });
+
+      if (!imageResult.canceled && imageResult.assets) {
+        setSelectedImages((prevImages) => [
+          ...prevImages,
+          ...imageResult.assets.map((asset) => ({
+            uri: asset.uri,
+            base64: asset.base64 || "",
+          })),
+        ]);
+      }
+      setModalVisible(true);
+    } catch (error) {
+      console.log("Error selecting images: ", error);
+    } finally {
+      loadingHandler(false);
+    }
+  }
+
+  async function uploadImages() {
+    loadingHandler(true);
+    try {
+      const results = await Promise.all(
+        selectedImages.map((image) =>
+          axios
+            .post(`${URI}/image`, {
+              file: image.base64,
+              label: label,
+              email: email,
+            })
+            .then((res: AxiosResponse<result>) => res.data)
+        )
+      );
+      console.log(results);
       //@ts-ignore
       await navigation.navigate("Result", {
-        result: result,
+        result: results,
         label: label,
       });
     } catch (error) {
-      console.error("error uploading image: ", error);
+      console.error("Error uploading images: ", error);
     } finally {
       loadingHandler(false);
     }
@@ -109,10 +145,16 @@ const CameraButton = ({ label, email, loadingHandler }: Props) => {
 
   function showImagePickerOptions() {
     Alert.alert("Select Image", "Choose an image source", [
-      { text: "Camera", onPress: takeImageHandler },
-      { text: "Gallery", onPress: pickImageHandler },
+      { text: "Camera", onPress: takeImagesWithCamera },
+      { text: "Gallery", onPress: selectImagesFromGallery },
       { text: "Cancel", style: "cancel" },
     ]);
+  }
+
+  function removeImage(uri: string) {
+    setSelectedImages((prevImages) =>
+      prevImages.filter((image) => image.uri !== uri)
+    );
   }
 
   return (
@@ -126,8 +168,56 @@ const CameraButton = ({ label, email, loadingHandler }: Props) => {
         <Image
           style={{ width: 60, height: 60 }}
           source={require("../assets/images/camera.png")}
-        ></Image>
+        />
       </Pressable>
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={[styles.modalContent, { height: 250 }]}>
+            <TouchableOpacity
+              onPress={() => setModalVisible(false)}
+              style={styles.closeButton}
+            >
+              <Text style={styles.closeButtonText}>X</Text>
+            </TouchableOpacity>
+            <ScrollView
+              horizontal
+              style={{ flex: 1 }}
+              contentContainerStyle={styles.scrollViewContent}
+            >
+              {selectedImages.map((image, index) => (
+                <View key={index} style={styles.imageContainer}>
+                  <Image
+                    source={{ uri: image.uri }}
+                    style={styles.imagePreview}
+                  />
+                  <TouchableOpacity
+                    style={styles.deleteButton}
+                    onPress={() => removeImage(image.uri)}
+                  >
+                    <Text style={styles.deleteButtonText}>X</Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+              <TouchableOpacity
+                style={[styles.imageContainer, styles.addButtonContainer]}
+                onPress={showImagePickerOptions}
+              >
+                <Text style={styles.addButtonText}>+</Text>
+              </TouchableOpacity>
+            </ScrollView>
+            <View style={styles.uploadButtonContainer}>
+              <Pressable onPress={uploadImages} style={styles.uploadButton}>
+                <Text style={styles.uploadButtonText}>Upload Images</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -139,6 +229,84 @@ const styles = StyleSheet.create({
   },
   pressed: {
     opacity: 0.75,
+  },
+  imageContainer: {
+    position: "relative",
+    margin: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 100,
+    height: 100,
+  },
+  imagePreview: {
+    width: 100,
+    height: 100,
+  },
+  deleteButton: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    backgroundColor: "red",
+    borderRadius: 15,
+    width: 30,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  deleteButtonText: {
+    color: "white",
+    fontSize: 18,
+  },
+  addButtonContainer: {
+    borderWidth: 2,
+    borderColor: "#d3d3d3",
+    borderRadius: 10,
+  },
+  addButtonText: {
+    fontSize: 40,
+    color: "#d3d3d3",
+  },
+  uploadButton: {
+    backgroundColor: "#42AF4D",
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+  },
+  uploadButtonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 10,
+    alignItems: "center",
+  },
+  scrollViewContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    zIndex: 1,
+  },
+  closeButtonText: {
+    fontSize: 24,
+  },
+  uploadButtonContainer: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
   },
 });
 
